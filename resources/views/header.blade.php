980bc6eefa1c4fbcce05ac44837c8b0ccca6ceb9
Beardyyy <hermes.technic@gmail.com>
2021-12-02 23:22:17 +0100

    Inserting Alpine JS

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,43 +11,23 @@
         I'm going to keep you guys up to speed with what's going on!
     </p>
 
-    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+    <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
 
 
-        <button>Categories</button>
+        <div x-data="{ show : false }" class="py-2 pl-3 pr-9 text-sm font-semibold">
 
-        <div>
-            <a href="#">One</a>
-            <a href="#">Two</a>
-            <a href="#">Three</a>
+        <button @click="show = !show">Categories</button>
+
+        <div x-show="show">
+            <a href="#" class="block text-left px-3 text-sm leading-5">One</a>
+            <a href="#" class="block text-left px-3 text-sm leading-5">Two</a>
+            <a href="#" class="block text-left px-3 text-sm leading-5">Three</a>
+        </div>
+
         </div>
 
 
 
-        <!--
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category</option>
-
-                @foreach($categories as $category)
-
-                    <a href="category/{{ $category->slug }}"> <option value="{{ $category->slug }}">{{ $category->name }} </option></a>
-
-                @endforeach
-
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>
-
-            -->
         </div>
 
         <!-- Other Filters -->
